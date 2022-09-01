6f28820296eca65157c89e1edb62142f8acebacc
alip putra <asalip.putra@gmail.com>
2022-09-01 15:45:03 +0700

    add nested directory category item form

--- nested_directory_admin.js
+++ nested_directory_admin.js
@@ -2,13 +2,56 @@ jQuery(document).ready(function()
 {
 
   var pluginPath = "../wp-content/plugins/nested-directory/";
+  var table = null;
 
-  function reloadNestedDirectory() {
-    var table = jQuery('.nd-table').DataTable({
+  jQuery('.nd-table').on('click','.editND',function(){
+    var id = jQuery(this).data('id');
+    console.log('edit', id)
+  });
+
+  jQuery('.nd-table').on('click','.deleteND',function(){
+    var id = jQuery(this).data('id');
+    console.log('delete', id)
+  });
+
+  jQuery('#nd-submit').click(function() {
+    var title = jQuery('#nd-tree-title').val();
+    var description = jQuery('#nd-tree-description').val();
+    var parent = jQuery('#nd-tree-parent').val();
+
+    if (title == "") {
+      jQuery('#nd-tree-title').addClass('nd-error');
+      setTimeout(function() {
+        jQuery('#nd-tree-title').removeClass('nd-error');
+      }, 1000);
+      return false;
+    }
+
+    var postForm = {
+      title: title,
+      parent: parent,
+      description: description,
+    };
+    
+    jQuery.ajax({
+      url: pluginPath + "nested-directory-form-tree.php?action=post_tree",
+      method:"POST",
+      data: postForm,
+      dataType: "json",      
+      success: function(data) {
+        reloadNestedDirectory();
+        self.parent.tb_remove();
+      }
+    });
+  });
+
+  function reloadTableNestedDirectory() {
+    table = jQuery('.nd-table').DataTable({
       "processing": true,
       "serverSide": true,
+      "bDestroy": true,
       "ajax":{
-               "url": pluginPath + "nested-directory-item.php?id=1&action=table_data",
+               "url": pluginPath + "nested-directory-item.php?id=0&action=table_data",
                "dataType": "json",
                "type": "POST"
              },
@@ -19,35 +62,29 @@ jQuery(document).ready(function()
           { "data": "actions" }
       ]  
     });
+  }
 
-    jQuery('.nd-table').on('click','.editND',function(){
-      var id = jQuery(this).data('id');
-      console.log('edit', id)
-    });
-
-    jQuery('.nd-table').on('click','.deleteND',function(){
-      var id = jQuery(this).data('id');
-      console.log('delete', id)
-    });
-    
-
+  function reloadNestedDirectory() {
     jQuery.ajax({
       url: pluginPath + "nested-directory-tree.php?action=item_data",
       method:"POST",
       dataType: "json",      
       success: function(data) {
-        jQuery('#nd-treeview').treeview({data: data})
+        reloadTableNestedDirectory();
+        jQuery('#nd-treeview').treeview({data: [{
+            "id": "1",
+            "name": "Main",
+            "text": "Main",
+            "parent_id": null,
+        }]})
         .on('nodeSelected', function(e, node){
             table.ajax.url(pluginPath + 'nested-directory-item.php?id='+node.id+'&action=table_data').load();
-            // console.log(node)
         });
       }  
     });
-
-    
   }
 
 
-  reloadNestedDirectory(0);
+  reloadNestedDirectory();
 
 });
